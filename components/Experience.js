cb1ee3364f9177189216cbe42153cb257be65b3b
Abdul Waris <abdulwarisaw532@gmail.com>
2024-01-15 17:15:41 -0500

    Adding bit bargain to portfolio

--- components/Experience.js
+++ components/Experience.js
@@ -12,20 +12,20 @@ export default function Experience() {
                         </div>
                         <figure className="texts">
                             <h3>Student</h3>
-                            <h4>App Academy</h4>
-                            <figcaption>Currently undertaking a comprehensive Full-Stack development program at App Academy, a highly esteemed institution with a rigorous selection process that accepts less than 5% of its applicants.</figcaption>
+                            <h4>App Academy, San Francisco</h4>
+                            <figcaption>Currently undertaking a comprehensive Full-Stack development program at App Academy, a highly esteemed institution with a rigorous selection process that accepts less than 10% of its applicants.</figcaption>
                         </figure>
                     </div>
                     <div className="item">
-                        <time className="date"><span className="year">2021</span><span className="arrow"></span></time>
+                        <time className="date"><span className="year">2022</span><span className="arrow"></span></time>
                         <div className="separator">
                             <div className="dot"></div>
                             <div className="line"></div>
                         </div>
                         <figure className="texts">
                             <h3>Front-End Developer</h3>
-                            <h4>Freelance, remote</h4>
-                            <figcaption>Collaborated with several clients on Upwork, providing Front-End Development services utilizing HTML, CSS, and JavaScript.</figcaption>
+                            <h4>Contracts, Virginia</h4>
+                            <figcaption>Worked with several SaaS businesses, providing Front-End Development services utilizing React, Tailwind & SCSS.</figcaption>
                         </figure>
                     </div>
                 </div>
